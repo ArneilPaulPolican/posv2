77c31a5a7131bba8199dc3fb9707aca055b50181
Arneil Paul Polican <policanarneilpaul@gmail.com>
2024-08-30 15:57:47 +0800

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'posv2',
+  webDir: 'dist'
+};
+
+export default config;
